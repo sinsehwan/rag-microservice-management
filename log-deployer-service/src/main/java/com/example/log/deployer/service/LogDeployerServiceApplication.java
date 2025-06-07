5ba2f45fdfeaa8457bd22b6088349eaad042196d
sinsehwan <sk124590@gmail.com>
2025-06-07 20:35:14 +0900

    Init: log-deplyer-service init setting

---
+++ log-deployer-service/src/main/java/com/example/log/deployer/service/LogDeployerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.log.deployer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDeployerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogDeployerServiceApplication.class, args);
+	}
+
+}
---
+++ log-deployer-service/src/test/java/com/example/log/deployer/service/LogDeployerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.log.deployer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogDeployerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
